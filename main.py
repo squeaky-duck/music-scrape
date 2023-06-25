48b7fcbb582d6ddc297d4d51069563d93ff55000
sqeaky-duck <mehulj35@gmail.com>
2023-06-25 19:20:07 +0530

    placeholder logic for store data, send email

--- main.py
+++ main.py
@@ -18,7 +18,26 @@ def extract(source):
     return value
 
 
+def send_email():
+    print("Email was sent!")
+
+
+def store(extracted_local):
+    with open("data.txt", "a") as file:
+        file.write(extracted_local + "\n")
+
+
+def read(extracted_local):
+    with open("data.txt", "r" ) as file:
+        return file.read()
+
+
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
+    content = read(extracted)
+    if extracted != "No upcoming tours":
+        if extracted not in content:
+            store(extracted)
+            send_email()
